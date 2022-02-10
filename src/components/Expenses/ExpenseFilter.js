7d1a17663ed2ec9f49d70647ff59e45bf8bba2f9
Christiaan van der Woude <ch_vdw@outlook.be>
2022-02-10 16:59:03 +0100

    working on expensefilter

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -1,17 +1,20 @@
-import React from 'react';
-
-import './ExpenseFilter.css';
+import React from "react";
+import "./ExpenseFilter.css";
 
 const ExpensesFilter = () => {
+  const onSelectHandler = (e) => {
+    console.log(e.target.value);
+  };
+
   return (
-    <div className='expenses-filter'>
-      <div className='expenses-filter__control'>
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select>
-          <option value='2022'>2022</option>
-          <option value='2021'>2021</option>
-          <option value='2020'>2020</option>
-          <option value='2019'>2019</option>
+        <select onSelect={onSelectHandler}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
         </select>
       </div>
     </div>
@@ -19,3 +22,5 @@ const ExpensesFilter = () => {
 };
 
 export default ExpensesFilter;
+
+// chosen year is lifted up to the expenses component from expense filter and store it in state there
